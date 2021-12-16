c2f48fb145d715af85049053f19101b4aa51e3c1
pavanmuttange <88590475+pavanmuttange@users.noreply.github.com>
2021-12-16 15:12:11 +0530

    commit

--- src/components/Covid.jsx
+++ src/components/Covid.jsx
@@ -1,23 +1,7 @@
 import React ,{useState,useEffect}from "react";
 import './covid.css';
 
-const Covid=()=>{
-    const [data, setData]=useState();
-    const getCovidData=async()=>{
-        try{
-            const res = await fetch('https://data.covid19india.org/data.json');
-            const actualData= await res.json();
-            console.log(actualData.statewise[0]);
-            setData(actualData.statewise[0]);
-            
-        }catch(err){
-            console.log(err);
-        }
-    }
-    useEffect(() => {
-        getCovidData();
-     }, []);
-    
+
     return(
         <div>
         <section>
