6a991284074975962ffc79a6c115bfe3954efa09
pavanmuttange <88590475+pavanmuttange@users.noreply.github.com>
2021-12-16 15:10:28 +0530

    first commit

--- src/components/Covid.jsx
+++ src/components/Covid.jsx
@@ -23,32 +23,7 @@ const Covid=()=>{
         <section>
         <h1>🔴LIVE</h1>
     <h2 className="mh2">COVID-19 CORONAVIRUS TRACKER</h2>
-    <div className="grid-container">
-    <div className="grid-item1">
-    <p><span>OUR</span> COUNTRY</p>
-    <p>INDIA</p>
-    </div>
-    <div className="grid-item2">
-    <p><span>TOTAL</span> CONFIRMED</p>
-    <p>{data.confirmed}</p>
-    </div>
-    <div className="grid-item3">
-    <p><span>TOTAL</span> RECOVERED</p>
-    <p>{data.recovered}</p>
-    </div>
-    <div className="grid-item4">
-    <p><span>TOTAL</span> ACTIVE</p>
-    <p>{data.active}</p>
-    </div>
-    <div className="grid-item5">
-    <p><span>TOTAL</span> DEATHS</p>
-    <p>{data.deaths}</p>
-    </div>
-    <div className="grid-item6">
-    <p><span>LAST</span> UPDATED</p>
-    <p>{data.lastupdatedtime}</p>
-    </div>
-    </div>
+    
     </section>
     </div>
     );
